284c290691496795f3429a6d31096728ffa0544e
Peter Burza <peterburza48@egmail.com>
2025-06-06 07:20:02 +0200

    implementation of TodoList behavior, setted TodoCard

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,25 @@
 import { TodoCard } from "./TodoCard";
 
-export function TodoList() {
+export function TodoList(props) {
+    const { todos } = props
 
-    return(
-        <div>
-            <TodoCard />
-        </div>
+    const tab = 'Open'
+    const filteredTodosList = tab === 'All' ?
+        todos :
+        tab === 'Open' ?
+            todos.filter(val => !val.complete) :
+            todos.filter(val => val.complete)
+
+    return (
+        <>
+            {filteredTodosList.map((todo, todoIndex) => {
+                return (
+                    <TodoCard
+                        key={todoIndex}
+                        todo={todo}
+                    />
+                )
+            })}
+        </>
     )
 }
