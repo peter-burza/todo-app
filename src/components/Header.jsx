b8e5b2fdffeb103014324f2b836f801281af9370
Peter Burza <peterburza48@egmail.com>
2025-06-06 15:22:18 +0200

    Managing opened tasks counter fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 export function Header(props) {
     const { todos } = props
-    const todoLength = todos.length
+    // const todoLength = todos.length
+    const openedTodosLength = todos.filter(val => !val.complete).length
 
-    const isTasksPlural = todos.length != 1
+    const isTasksPlural = openedTodosLength != 1
 
     const taskOrTasks = isTasksPlural ? 'tasks' : 'task'
 
     return (
         <header>
-            <h1 className="text-gradient">You have {todoLength} open {taskOrTasks}.</h1>
+            <h1 className="text-gradient">You have {openedTodosLength} open {taskOrTasks}.</h1>
         </header>
     )
 }
